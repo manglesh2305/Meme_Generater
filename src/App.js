bcec08f07815fd7a042a63b6867da33eaa301548
Manglesh <mangleshyadav5456@gmail.com>
2023-08-05 18:25:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./component/header";
+import Meme from "./component/meme";
+import "./styles.css";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
---
+++ src/component/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./images/troll-face.png" className="header--image" alt="img" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Project </h4>
+    </header>
+  );
+}
